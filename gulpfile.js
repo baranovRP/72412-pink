528a6232f5ba24a47965c9e8c19d4b8f3e191466
Roman Baranov <baranov.r.p@gmail.com>
2015-11-15 20:01:00 +0100

    #trunk: add webserver and livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 "use strict";
 
 var gulp = require("gulp");
+var webserver = require("gulp-webserver");
 
-
+gulp.task("webserver", function() {
+  gulp.src("./")
+    .pipe(webserver({
+      fallback: "index.html",
+      livereload: true,
+      open: true,
+      directoryListing: {
+        enable: true,
+        path: "public"
+      }
+    }));
+});
 
 // Оставьте эту строку в самом конце файла
 require("./.gosha");
