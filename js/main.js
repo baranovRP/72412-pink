3d136eef5359c5c6dcfdba60ebfe0461bac57ec0
Roman Baranov <baranov.r.p@gmail.com>
2015-11-24 00:11:59 +0100

    #module3-task1: add page-address-footer

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/* contact-map */
+function initMap() {
+
+  var map = new google.maps.Map(document.getElementById("page-map"), {
+    zoom: 17,
+    scrollwheel: false,
+    center: {
+      lat: 59.938794,
+      lng: 30.323083
+    }
+
+  });
+
+  var contactLatLng = {
+    lat: 59.938794,
+    lng: 30.323083
+  };
+
+  var contactLogo = {
+    url: "img/icon-map-pointer.png"
+  };
+
+  var marker = new google.maps.Marker({
+    position: contactLatLng,
+    icon: contactLogo,
+    draggable: true,
+    map: map
+  });
+
+}
