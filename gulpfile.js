9bf649164c1fb4965966fa50132eef23fa433cb5
Roman Baranov <baranov.r.p@gmail.com>
2015-12-04 22:10:34 +0100

    #module5-task1: fix indent

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var webserver = require("gulp-webserver");
 
 gulp.task("style", function() {
   return gulp.src("sass/style.{sass,scss}")
@@ -17,21 +16,8 @@ gulp.task("style", function() {
     .pipe(gulp.dest("css"));
 });
 
-gulp.task("webserver", function() {
-  gulp.src("./")
-    .pipe(webserver({
-      fallback: "index.html",
-      livereload: true,
-      open: true,
-      directoryListing: {
-        enable: true,
-        path: "public"
-      }
-    }));
-});
-
-gulp.task("start", ["style", "webserver"], function() {
-  gulp.watch("sass/**/*.{sass,scss}", ["style"], ["webserver"]);
+gulp.task("start", ["style"], function() {
+  gulp.watch("sass/**/*.{sass,scss}", ["style"]);
 });
 
 
