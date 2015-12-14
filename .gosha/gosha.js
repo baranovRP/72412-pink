455ef20b3d4cd14df8bfeffe912b5b69884ff75c
Roman Baranov <baranov.r.p@gmail.com>
2015-12-14 20:14:15 +0100

    #module9-task1: update with fonts folder

--- .gosha/gosha.js
+++ .gosha/gosha.js
@@ -4,7 +4,7 @@ var fs = require("fs-extra");
 var gulp = require("gulp");
 
 function copy() {
-  return gulp.src(["*.html", "{css,img,js}/**"])
+  return gulp.src(["*.html", "{css,img,js,fonts}/**"])
     .pipe(gulp.dest("gosha"));
 }
 
