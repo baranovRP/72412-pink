db27480ca1a1eb0cf4f731f41e844819a30f81c3
Roman Baranov <baranov.r.p@gmail.com>
2015-12-16 23:12:09 +0100

    #module10-task1: build automation

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,140 @@
 "use strict";
 
 var gulp = require("gulp");
-var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
+var clean = require("gulp-clean");
+var rename = require("gulp-rename");
 
-gulp.task("style", function() {
-  return gulp.src("sass/style.{sass,scss}")
+var sass = require("gulp-sass");
+var combineMq = require("gulp-combine-mq");
+var postCss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var minifyCss = require("gulp-minify-css");
+var minifyJs = require("gulp-uglify");
+var imgOptim = require("gulp-image-optimization");
+var minifyHtml = require("gulp-minify-html");
+
+var watch = require("gulp-watch");
+var webserver = require("gulp-webserver");
+
+
+var bases = {
+  source: "source/",
+  build: "build/"
+};
+
+
+var paths = {
+  fonts: ["fonts/**/*.{woff,woff2}"],
+  styles: ["sass/**/*.{sass,scss}"],
+  style: ["sass/style.{sass,scss}"],
+  images: ["img/**/*.{jpg,png,svg}"],
+  scripts: ["js/**/*.js"],
+  html: ["*.html"]
+};
+
+
+/* [DEV] */
+gulp.task("webserver", function() {
+  gulp.src("./")
+    .pipe(webserver({
+      fallback: "index.html",
+      livereload: true,
+      open: true,
+      directoryListing: {
+        enable: true,
+        path: "public"
+      }
+    }));
+});
+
+
+gulp.task("style--dev", function() {
+  return gulp.src("sass/style.{sass,scss}", {cwd: bases.source})
     .pipe(plumber())
     .pipe(sass())
-    .pipe(postcss([
+    .pipe(postCss([
       autoprefixer({browsers: "last 2 versions"})
     ]))
-    .pipe(gulp.dest("css"));
+    .pipe(gulp.dest(bases.source + "css"));
 });
 
-gulp.task("start", ["style"], function() {
-  gulp.watch("sass/**/*.{sass,scss}", ["style"]);
+
+gulp.task("start", ["style--dev", "webserver"], function() {
+  gulp.watch("source/sass/**/*.{sass,scss}", ["style--dev"]);
 });
 
+
+/* [PROD] */
+gulp.task("clean", function() {
+  return gulp.src(bases.build)
+    .pipe(clean());
+});
+
+
+gulp.task("fonts", ["clean"], function() {
+  gulp.src(paths.fonts, {cwd: bases.source})
+    .pipe(gulp.dest(bases.build + "fonts"))
+});
+
+
+gulp.task("style", ["clean"], function() {
+  return gulp.src(paths.style, {cwd: bases.source})
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postCss([
+      autoprefixer({browsers: "last 2 versions"})
+    ]))
+    .pipe(gulp.dest(bases.build + "css"))
+    .pipe(combineMq())
+    .pipe(minifyCss())
+    .pipe(rename(function(path) {
+      path.basename += ".min";
+      return path;
+    }))
+    .pipe(gulp.dest(bases.build + "css"))
+    ;
+});
+
+
+gulp.task('imagemin', ['clean'], function() {
+  gulp.src(paths.images, {cwd: bases.source})
+    .pipe(imgOptim({
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true
+    }))
+    .pipe(gulp.dest(bases.build + "img"));
+});
+
+
+gulp.task("scripts", ["clean"], function() {
+  gulp.src(paths.scripts, {cwd: bases.source})
+    .pipe(plumber())
+    .pipe(gulp.dest(bases.build + "js"))
+    .pipe(minifyJs())
+    .pipe(rename(function(path) {
+      path.basename += ".min";
+      return path;
+    }))
+    .pipe(gulp.dest(bases.build + "js"));
+});
+
+
+gulp.task("html", ["clean"], function() {
+  gulp.src(paths.html, {cwd: bases.source})
+    .pipe(plumber())
+    .pipe(gulp.dest(bases.build))
+    .pipe(minifyHtml())
+    .pipe(rename(function(path) {
+      path.basename += ".min";
+      return path;
+    }))
+    .pipe(gulp.dest(bases.build));
+});
+
+
+gulp.task("build", ["clean", "fonts", "style", "imagemin", "scripts", "html"]);
 
 
 // Оставьте эту строку в самом конце файла
